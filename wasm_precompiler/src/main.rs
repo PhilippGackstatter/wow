68b62e9c1c577bb24e2ef8f50e9d88ffc9f9aed5
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2021-01-20 10:35:41 +0100

    precompile: Implement wasmer and cli arg parsing

--- wasm_precompiler/src/main.rs
+++ wasm_precompiler/src/main.rs
@@ -1,39 +1,84 @@
 use anyhow;
+use clap::{App, Arg, SubCommand};
 use std::{fs::File, io::prelude::*, path::Path, time::Instant};
-use wasmtime::{Module, Store};
+
+const WASMTIME: &'static str = "wasmtime";
+const WASMER: &'static str = "wasmer";
 
 fn main() -> anyhow::Result<()> {
-    let mut args = std::env::args();
+    let matches = App::new("Wasm Precompiler")
+        .version("0.1")
+        .author("Philipp Gackstatter")
+        .about("Precompiles Wasm modules to runtime-specific code")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .subcommand(SubCommand::with_name(WASMTIME).about("Precompiles for wasmtime"))
+        .subcommand(SubCommand::with_name(WASMER).about("Precompiles for wasmer"))
+        .get_matches();
 
-    args.next();
+    let filename = matches.value_of("INPUT").unwrap();
 
-    let filename = args.next().unwrap();
+    match matches.subcommand_name() {
+        Some(WASMTIME) => precompile(filename, precompile_wasmtime, WASMTIME),
+        Some(WASMER) => precompile(filename, precompile_wasmer, WASMER),
+        _ => panic!("No subcommand provided"),
+    }
+}
 
-    let pre_wasmtime = precompile_wasmtime(&filename)?;
+fn precompile<F: FnOnce(&str) -> anyhow::Result<Vec<u8>>>(
+    filename: &str,
+    precompile_fn: F,
+    runtime_name: &'static str,
+) -> anyhow::Result<()> {
+    let precompiled_bytes = precompile_fn(&filename)?;
 
-    write_precompiled(filename, pre_wasmtime)?;
+    write_precompiled(filename, runtime_name, precompiled_bytes)?;
 
     Ok(())
 }
 
 fn precompile_wasmtime(filename: &str) -> anyhow::Result<Vec<u8>> {
-    let store = Store::default();
+    let store = wasmtime::Store::default();
 
     let timestamp = Instant::now();
 
-    let module = Module::from_file(store.engine(), filename).unwrap();
+    let module = wasmtime::Module::from_file(store.engine(), filename).unwrap();
 
     println!("Precompiling took {}ms", timestamp.elapsed().as_millis());
 
     module.serialize()
 }
 
-fn write_precompiled(filename: String, precompiled: Vec<u8>) -> anyhow::Result<()> {
+fn precompile_wasmer(filename: &str) -> anyhow::Result<Vec<u8>> {
+    // let compiler = LLVM::default();
+
+    // let store = Store::new(&JIT::new(compiler).engine());
+
+    let store = wasmer::Store::default();
+
+    let before = Instant::now();
+    let module = wasmer::Module::from_file(&store, filename)?;
+
+    println!("wasmer compiling took {}ms", before.elapsed().as_millis());
+
+    Ok(module.serialize()?)
+}
+
+fn write_precompiled(
+    filename: &str,
+    runtime_name: &'static str,
+    precompiled: Vec<u8>,
+) -> anyhow::Result<()> {
     let file_path = Path::new(&filename);
 
     let mut new_filepath = file_path.parent().unwrap().to_owned();
 
-    let new_file_stem = file_path.file_stem().unwrap().to_string_lossy().to_string() + ".wasmtime";
+    let new_file_stem =
+        file_path.file_stem().unwrap().to_string_lossy().to_string() + "." + runtime_name;
 
     new_filepath.push(new_file_stem);
 
