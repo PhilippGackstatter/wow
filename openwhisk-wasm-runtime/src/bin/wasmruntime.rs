7f7fd605c0a6e34e1dca9672f2fe49894e5bb60f
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2021-01-26 11:59:39 +0100

    Add tracing to tide for better debugging

--- openwhisk-wasm-runtime/src/bin/wasmruntime.rs
+++ openwhisk-wasm-runtime/src/bin/wasmruntime.rs
@@ -3,12 +3,21 @@ extern crate tide;
 use async_std::sync::RwLock;
 use openwhisk_wasm_runtime::core;
 use std::{collections::HashMap, sync::Arc};
+use tide_tracing::TraceMiddleware;
+use tracing::Level;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state = Arc::new(RwLock::new(HashMap::new()));
 
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("no global subscriber has been set");
+
     let mut app = tide::with_state(state);
+    app.with(TraceMiddleware::new());
 
     app.at("/start").post(core::start);
     app.at("/init").post(core::init);
