977dc1e9003186288c058d8eaa6320d205e2b176
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2021-03-01 17:31:40 +0100

    examples: Simplify filesys

--- wasm-json/examples/filesys.rs
+++ wasm-json/examples/filesys.rs
@@ -1,6 +1,3 @@
-use rand::{Rng, SeedableRng};
-use rand_xoshiro::Xoshiro256StarStar;
-
 #[cfg(feature = "wasm")]
 wasm_json::pass_json!(func);
 
@@ -9,30 +6,16 @@ wasm_json::json_args!(func);
 
 // Needs to be created with --annotation dir "/tmp/filesys"
 
-const NUM_BYTES: usize = 5_000_000;
 const PATH: &'static str = "/tmp/filesys/test.txt";
 
 pub fn func(_json: serde_json::Value) -> Result<serde_json::Value, anyhow::Error> {
     let generation_time = std::time::Instant::now();
-    let mut random_bytes: Vec<u8> = Vec::with_capacity(NUM_BYTES);
-
-    let mut rng = Xoshiro256StarStar::seed_from_u64(0);
-
-    for i in 0..NUM_BYTES {
-        random_bytes.push(rng.gen());
-    }
-
-    let generation_time = generation_time.elapsed().as_millis();
-
     let write_read_time = std::time::Instant::now();
-
-    std::fs::write(PATH, &random_bytes)?;
 
     let read_bytes = std::fs::read(PATH)?;
 
     Ok(serde_json::json!({
-        "success": read_bytes == random_bytes,
-        "generation_time": generation_time as u64,
+        "success": true,
         "write_read_time": write_read_time.elapsed().as_millis() as u64,
     }))
 }
