37c9f30a2165f0bebfac6c200fa7db492d8d23cc
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2021-03-27 22:25:19 +0100

    filesys: Base name on timestamp & use OpenOptions

--- wasm-json/examples/filesys.rs
+++ wasm-json/examples/filesys.rs
@@ -1,4 +1,7 @@
-use std::io::Write;
+use std::{
+    fs::OpenOptions,
+    io::{Read, Write},
+};
 
 #[cfg(feature = "wasm")]
 wasm_json::pass_json!(func);
@@ -8,20 +11,32 @@ wasm_json::json_args!(func);
 
 // Needs to be created with --annotation dir "/tmp/filesys"
 
-const PATH: &'static str = "/tmp/filesys/test.txt";
-
 pub fn func(_json: serde_json::Value) -> Result<serde_json::Value, anyhow::Error> {
     // let generation_time = std::time::Instant::now();
+    let path = format!(
+        "/tmp/filesys/{}.txt",
+        std::time::SystemTime::now()
+            .duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .expect("Time went backwards.")
+            .as_secs()
+    );
+
     let write_read_time = std::time::Instant::now();
 
-    let mut file = std::fs::File::create(PATH).unwrap();
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&path)?;
 
     file.write_all(b"Hello, Wasm.").unwrap();
 
-    let _read_bytes = std::fs::read(PATH)?;
+    let mut res = String::new();
+    file.read_to_string(&mut res).unwrap();
 
     Ok(serde_json::json!({
         "success": true,
         "write_read_time": write_read_time.elapsed().as_millis() as u64,
+        "content": res
     }))
 }
