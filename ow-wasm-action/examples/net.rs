ce9e43382e586249b81c33dfde0cb64c3648e3f8
PhilippGackstatter <philippgackstatter@web.de>
2021-04-07 18:00:45 +0200

    net: Sleep instead of get() for now

--- ow-wasm-action/examples/net.rs
+++ ow-wasm-action/examples/net.rs
@@ -4,15 +4,15 @@ ow_wasm_action::pass_json!(func);
 #[cfg(feature = "bin")]
 ow_wasm_action::json_args!(func);
 
-#[link(wasm_import_module = "http")]
-extern "C" {
-    fn get() -> i32;
-}
+// #[link(wasm_import_module = "http")]
+// extern "C" {
+//     fn get() -> i32;
+// }
 
 pub fn func(_json: serde_json::Value) -> Result<serde_json::Value, anyhow::Error> {
     let request_time = std::time::Instant::now();
 
-    let _req = unsafe { get() };
+    let _req = std::thread::sleep(std::time::Duration::new(0, 300_000_000)); //unsafe { get() };
 
     Ok(serde_json::json!({
         "request_time": request_time.elapsed().as_millis() as u64,
