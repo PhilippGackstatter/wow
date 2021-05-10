dc62fc4dbbd655f97ecf8d6ca3d59ee22341439d
PhilippGackstatter <philippgackstatter@web.de>
2021-05-10 10:03:19 +0200

    examples: Add sleep to block if not wasm

--- ow-wasm-action/examples/block.rs
+++ ow-wasm-action/examples/block.rs
@@ -4,9 +4,16 @@ ow_wasm_action::pass_json!(func);
 #[cfg(not(feature = "wasm"))]
 ow_wasm_action::json_args!(func);
 
+#[cfg(feature = "wasm")]
 #[link(wasm_import_module = "http")]
 extern "C" {
     fn get() -> i32;
+}
+
+#[cfg(not(feature = "wasm"))]
+unsafe fn get() -> i32 {
+    std::thread::sleep(std::time::Duration::new(0, 300_000_000));
+    0
 }
 
 pub fn func(_json: serde_json::Value) -> Result<serde_json::Value, anyhow::Error> {
